a0fc7d4fa1aec08f931bae9685f1044ab26a9bea
Anzhelika Smirnova <Anzhelika_Smirnova@epam.com>
2020-11-18 18:56:45 +0300

    feat: init structure

---
+++ MyProjects/movies/webpack.config.js
@@ -0,0 +1,95 @@
+const path = require('path');
+//const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = (env, options) => {
+    const isProduction = options.mode === 'production'
+    const config = {
+        mode: isProduction ? 'production' : 'development',
+        entry: ['./src/style.css', './src/index.js'],
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, './dist'),
+        },
+        devServer: {
+            overlay: true,
+            port: 8080,
+        },
+        devtool: isProduction ? 'none' : 'source-map',
+        watch: !isProduction,
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader, 'sass-loader'
+                    ]
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.(png|svg|jpe?g|gif)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                        },
+
+                    ],
+                },
+                {
+                    test: /\.html$/,
+                    loader: 'html-loader',
+                },
+                {
+                    enforce: 'pre',
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'eslint-loader',
+                    options: {
+                        fix: true,
+                    }
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                }
+
+            ]
+        },
+        plugins: [
+            //new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: 'index.html'
+            }
+            ),
+            new MiniCssExtractPlugin({
+                filename: 'style.css'
+            }),
+            new CopyWebpackPlugin({
+                patterns:
+                    [{ from: './src/assets', to: './assets' }]
+            })
+        ]
+    }
+    return config;
+};
